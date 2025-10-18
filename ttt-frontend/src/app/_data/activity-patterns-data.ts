47a6ae6080908a03da790a2b101d04619d8da705
Devvaru <syeraru@gmail.com>
2025-10-17 18:15:37 -0600

    Add test data with types

---
+++ ttt-frontend/src/app/_data/activity-patterns-data.ts
@@ -0,0 +1,7 @@
+import type { ActivityPattern } from "../_types/types.js";
+
+export const activityPatterns: ActivityPattern[] = [
+  { id: 0, title: "Feed At Target" },
+  { id: 1, title: "Catch From Feed" },
+  { id: 3, title: "Carry Along Path" }
+];
--- ttt-frontend/src/app/_types/types.ts
+++ ttt-frontend/src/app/_types/types.ts
@@ -3,3 +3,28 @@ export type DropdownOption = {
     label: string;
     description: string;
 };
+
+export interface ActivityPattern {
+    id: number;
+    title: string;
+};
+
+export interface ActivityOption {
+    id: number;
+    value: string;
+    title: string;
+    description: string;
+};
+
+export interface ActivityInfo {
+    id: number;
+    title: string;
+    description: string;
+}
+
+export interface ActivityElement {
+    id: number;
+    title: string;
+    options: ActivityOption[];
+    info?: ActivityInfo[];
+};
