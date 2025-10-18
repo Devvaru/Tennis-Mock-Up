b84e7e3540c465faa5a0f5e66abf706e0f9f6c7f
Devvaru <syeraru@gmail.com>
2025-10-18 17:13:45 -0600

    Update config and package.json for deployment

--- ttt-frontend/next.config.ts
+++ ttt-frontend/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: { unoptimized:true },
+  basePath: "/Tennis-Mock-Up",
+  assetPrefix: "/Tennis-Mock-Up/"
 };
 
 export default nextConfig;
